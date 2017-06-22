ce4cf439fc62d82d0a68d663ebb1f97117c0a4af
Breno Rodrigues <brenords@live.com>
2017-06-22 19:49:27 +0000

    Implemented basic functionality in the open_word_list method to read the word_list file and return a list with the words.

--- zipcracker.py
+++ zipcracker.py
@@ -5,11 +5,12 @@ class ZipCracker(object):
     def __init__(self, zip_file, word_list=None):
         #self.zip_file = self.open_zip_file(zip_file)
         self.zip_file = zip_file
-        #self.word_list = self.open_word_list(word_list)
-        self.word_list = word_list
+        self.word_list = self.open_word_list(word_list)
 
     def crack_zip(self, zip_file, wordlist):
         pass
 
-    def open_word_list(self, wordlist):
-        pass
+    def open_word_list(self, word_list):
+        wordlist_file = open(word_list, 'r')
+        wordlist = wordlist_file.read().split('\n')
+        return wordlist
