a8894129f7adcc606479d294254ee7f9d643f4ca
Breno Rodrigues <brenords@live.com>
2017-06-22 18:34:27 +0000

    Implementation of __init__ method. Using zip_file as attribute for the object. Importing 'string' and 'random' for use in the method create_word_list. Changing the parameters of the method create_word_list to add 'file_name' and removing the 'self' (thinking in making a @staticmethod).

--- zipcracker.py
+++ zipcracker.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import string
+import random
+
 class ZipCracker(object):
+
+    def __init__(self, zip_file):
+        self.zip_file = zip_file
 
     def crack_zip(self, zip_file, wordlist):
         pass
 
-    def create_word_list(self, min_lenght, max_lenght):
+    def create_word_list(file_name, min_lenght, max_lenght, max_words):
         pass
 
     def open_word_list(self, wordlist):
