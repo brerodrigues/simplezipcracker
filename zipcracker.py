91ca17318049d38bafee33bf9699cfe033ff2cb6
Breno Rodrigues <brenords@live.com>
2017-06-22 14:07:42 +0000

    Naming the basic methods and their parameters of class ZipCracker.

--- zipcracker.py
+++ zipcracker.py
@@ -1,2 +1,10 @@
 class ZipCracker:
-    pass
+
+    def crack_zip(self, zip_file, wordlist):
+        pass
+
+    def create_word_list(self, min_lenght, max_lenght):
+        pass
+
+    def open_word_list(self, wordlist):
+        pass
