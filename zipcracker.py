72695fdd79eebda7a7a3bc90dcf7d6a04ae5d589
Breno Rodrigues <brenords@live.com>
2017-06-27 13:36:48 +0000

    Renamed methods to be more consistent with their functions

--- zipcracker.py
+++ zipcracker.py
@@ -7,15 +7,15 @@ class ZipCracker(object):
     def __init__(self, zip_file, word_list):
         #self.zip_file = self.open_zip_file(zip_file)
         self.zip_file = zip_file
-        self.word_list = self.open_word_list(word_list)
+        self.word_list = self.process_word_list(word_list)
 
     def crack_zip(self, zip_file, wordlist):
         pass
 
-    def open_zip(self, zip_file):
+    def process_zip(self, zip_file):
         pass
 
-    def open_word_list(self, word_list):
+    def process_word_list(self, word_list):
         try:
             wordlist_file = open(word_list, 'r')
         except FileNotFoundError:
