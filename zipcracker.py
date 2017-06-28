9240ab07a19705e85fef549555a9ae3666443a9e
Breno Rodrigues <brenords@live.com>
2017-06-28 13:13:35 +0000

    Implemented process_zip to check if zipfile is a valid zip.

--- zipcracker.py
+++ zipcracker.py
@@ -5,15 +5,17 @@ import zipfile
 class ZipCracker(object):
 
     def __init__(self, zip_file, word_list):
-        #self.zip_file = self.open_zip_file(zip_file)
-        self.zip_file = zip_file
+        self.zip_file = self.process_zip(zip_file)
         self.word_list = self.process_word_list(word_list)
 
     def crack_zip(self, zip_file, wordlist):
         pass
 
     def process_zip(self, zip_file):
-        pass
+        if zipfile.is_zipfile(zip_file):
+            return(True, zip_file)
+        else:
+            return(False, '{} is not a valid zip file!'.format(zip_file))
 
     def process_word_list(self, word_list):
         try:
